ed2b8f114e41674ccd098a08ddf88f7b64914936
Mariana Lobo <m.lobocasas@gmail.com>
2023-02-24 11:24:52 +0100

    paint favorites cocktails to favorite list

--- src/js/main.js
+++ src/js/main.js
@@ -4,71 +4,106 @@
 
 console.log('>> Ready :)');
 
-//Variables
+//VARIABLES
 const inputSearch = document.querySelector('.js-input-search');
 const btnSearch = document.querySelector('.js-btn-search');
 const CocktailsList = document.querySelector('.js-ul-cocktails');
+const cocktailsListFav = document.querySelector('.js-list-favorites');
 let cocktails = [];
+let favoritesCocktails = [];
 
 
+//FUNCIONES PARA PINTAR
 
-//Función para pintar la lista de cocteles
+//Pintar la lista de cocteles
 function renderCocktailList() {
     CocktailsList.innerHTML = "";
-    for (const cocktail of cocktails){
-        //Creamos los elementos
-        const liCocktail = document.createElement('li');
-        const articleCocktail = document.createElement('article');
-        const h2Cocktail = document.createElement('h2');
-        const imgCocktail = document.createElement('img');
-        //Creamos los nodos
-        const CocktailName = document.createTextNode(cocktail.name);
-        //Creamos los atributos
-        liCocktail.setAttribute('class', 'js-li-cocktail');
-        imgCocktail.setAttribute('src', cocktail.image);
-        //Asignamos los padres a los elementos creados
-        h2Cocktail.appendChild(CocktailName);
-        articleCocktail.appendChild(h2Cocktail);
-        articleCocktail.appendChild(imgCocktail);
-        liCocktail.appendChild(articleCocktail);
-        CocktailsList.appendChild(liCocktail);
+    for (const cocktail of cocktails) {
+        CocktailsList.innerHTML += renderCocktail(cocktail);
     }
     addEventToList();
 }
+//Pintar la lista de favoritos
+function renderFavoriteList() {
+    cocktailsListFav.innerHTML = "";
+    for (const cocktail of favoritesCocktails) {
+        cocktailsListFav.innerHTML += renderCocktail(cocktail);
+    }
+}
+//Pintar un elemento de la lista
+function renderCocktail(cocktail) {
+    let html = `<li class="js-li-cocktail" id=${cocktail.id} >
+        <article >
+        <h2>${cocktail.name}</h3>
+        <img src=${cocktail.image}>
+        </article> 
+        </li> `;
+    return html;
+}
 
+//FUNCIONES GENERALES
 
-//Función para que aparezcan cocteles al cargar la página
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
+//Función para que aparezcan cocteles margarita al cargar la página
+getCocktails();
+function getCocktails() {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
     .then((response) => response.json())
     .then((data) => {
         cocktails = data.drinks.map((drinks) => ({
             name: drinks.strDrink,
             image: drinks.strDrinkThumb,
+            id: drinks.idDrink,
         }));
         renderCocktailList();
 });
+}
+
 
 //Función manejadora del evento del boton de buscar
 function handleClickSearch(ev) {
     ev.preventDefault();
     const inputSearchValue = inputSearch.value;
+    if (inputSearchValue === "") {
+        getCocktails();
+    } else {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputSearchValue}`)
         .then((response) => response.json())
         .then((data) => {
             cocktails = data.drinks.map((drinks) => ({
                 name: drinks.strDrink,
                 image: drinks.strDrinkThumb,
+                id: drinks.idDrink,
             }));
-            console.log(cocktails);
-            renderCocktailList();
+        console.log(cocktails);
+        renderCocktailList();
         });
+    } 
 }
 
-//Función manejadora del evento de los li de la lista
+//Función manejadora del evento de los li de la lista para que se seleccionen y se añadan a la lista de favoritos
 function handleClick(ev) {
     console.log('he hecho click');
     ev.currentTarget.classList.toggle('selected');
+    //busco por id en la lista de cocteles los que están seleccionados y los guardo en una variable
+    const idSelected = ev.currentTarget.id;
+    const cocktailSelected = cocktails.find(cocktail => cocktail.id === idSelected);
+    //Busca la posicion donde está el coctel en la lista de favoritos
+    const indexCocktail = favoritesCocktails.findIndex(cocktail => cocktail.id === idSelected);
+    //compruebo si ya está en favoritos, si no está nos devuelve -1
+    if (indexCocktail === -1) {
+        favoritesCocktails.push(cocktailSelected);
+    } else {
+        favoritesCocktails.splice(indexCocktail, 1);
+
+    }
+    console.log(favoritesCocktails);
+    renderFavoriteList();
+
 }
+
+// EVENTOS
+
+//Funcion para escuchar los eventos en cada li de la lista
 function addEventToList() {
     const liElements = document.querySelectorAll('.js-li-cocktail');
     for (const li of liElements) {
